0b30fbb36666bc6b0bb0b6ab7106c7fdca793634
mrlomax <mattylomax@googlemail.com>
2025-09-16 14:40:13 +0100

    WIP: Learning list methods

--- expense-tracker.py
+++ expense-tracker.py
@@ -1,5 +1,10 @@
 def main():
     my_list = [1, 2]
+    my_list.append(3)
+    my_list[0] = 0
+    my_list.insert(1, 1)
+    my_list.pop()
+    print(my_list)
 
 if __name__ == '__main__':
-    pass
+    main()
